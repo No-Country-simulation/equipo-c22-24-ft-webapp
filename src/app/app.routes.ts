e00f2090ac0b5e27599d4cd1d73cef851d5e3565
Carlos Fabian <110875305+chzfabian@users.noreply.github.com>
2024-12-04 22:40:22 -0300

    C22

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./shared/components/layout/layout.component'),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./business/dashboard/dashboard.component').then(m => m.DashboardComponent),
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./business/profile/profile.component').then(m => m.ProfileComponent),
+      },
+      {
+        path: 'tables',
+        loadComponent: () => import('./business/tables/tables.component').then(m => m.TablesComponent),
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  }
+];
